cda5a19d61aad7d02c74222418aefeae52931110
Ragavendiran <bgragavendiran@gmail.com>
2023-09-11 16:58:55 +0530

    updated keyboard code

--- UIForWaterQualityRevised/screens/config_screen.py
+++ UIForWaterQualityRevised/screens/config_screen.py
@@ -2,60 +2,44 @@ import tkinter as tk
 from .base_screen import BaseScreen
 import os
 from .config_handler import ConfigHandler
-from .USKeyboard import USKeyboard
+from .keyboard import Keyboard
 from tkinter import messagebox
 
 
-def show_keyboard(self, event):
-    self.keyboard.active_text_widget = event.widget
-    self.keyboard.deiconify()
 
 
 class ConfigPage(BaseScreen):
-    def show_keyboard(self, event):
-        if hasattr(self, "keyboard"):
-            self.keyboard.destroy()
-        self.keyboard = USKeyboard(self, active_text_widget=event.widget)
-        self.keyboard.place(anchor="s", relx=0.5, rely=1.0)
-
 
     def __init__(self, master, app_instance):
+        self.keyboard_instance = None
         super().__init__(master, "", app_instance, background="../images/config.png")
         self.current_directory = os.path.dirname(os.path.abspath(__file__))
         self.config_handler = ConfigHandler()
-        self.keyboard = USKeyboard(self)
-        self.keyboard.place(anchor="s", relx=0.5, rely=1.0)
-        self.keyboard.withdraw()
-
+        # self.keyboard.hide_keyboard()
 
 
         # Parent frame to hold the grid
         parent_frame = tk.Frame(self)
-        parent_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        parent_frame.place(relx=0.5, rely=0.35, anchor=tk.CENTER)
 
         low_level_frame = tk.Frame(parent_frame)
         low_level_frame.grid(row=0, column=0, padx=10, pady=10)
-
         low_level_label = tk.Label(low_level_frame, text="Threshold Delta Peak Counts")
         low_level_label.grid(row=0, column=0, padx=10, pady=10)
         low_level_label.bind("<Button-1>", lambda event: self.show_entry_widgets(self.threshold_delta_peak_edittext))
-
         self.threshold_delta_peak_edittext = tk.Entry(low_level_frame, name="threshold_delta_peak")
         self.threshold_delta_peak_edittext.bind('<FocusIn>', self.show_keyboard)
-        self.threshold_delta_peak_edittext.bind('<Button-1>', self.show_keyboard)
         self.threshold_delta_peak_edittext.grid(row=0, column=1, padx=10, pady=10)
         self.threshold_delta_peak_edittext.insert(0,
                                                   self.config_handler.get_threshold_delta_peak_counts())  # Set default value
 
         high_level_frame = tk.Frame(parent_frame)
         high_level_frame.grid(row=1, column=0, padx=10, pady=10)
-
         high_level_label = tk.Label(high_level_frame, text="Threshold Delta Total Counts")
         high_level_label.grid(row=0, column=0, padx=10, pady=10)
         high_level_label.bind("<Button-1>", lambda event: self.show_entry_widgets(self.threshold_delta_total_edittext))
         self.threshold_delta_total_edittext = tk.Entry(high_level_frame, name="threshold_delta_total")
         self.threshold_delta_total_edittext.bind('<FocusIn>', self.show_keyboard)
-        self.threshold_delta_total_edittext.bind('<Button-1>', self.show_keyboard)
         self.threshold_delta_total_edittext.grid(row=0, column=1, padx=10, pady=10)
         self.threshold_delta_total_edittext.insert(0,
                                                    self.config_handler.get_threshold_delta_total_counts())  # Set default value
@@ -70,48 +54,46 @@ class ConfigPage(BaseScreen):
 
         self.acquisition_duration_edittext = tk.Entry(acquisition_duration_frame, name="acquisition_duration")
         self.acquisition_duration_edittext.bind('<FocusIn>', self.show_keyboard)
-        self.acquisition_duration_edittext.bind('<Button-1>', self.show_keyboard)
         self.acquisition_duration_edittext.grid(row=0, column=1, padx=10, pady=10)
         self.acquisition_duration_edittext.insert(0,
                                                   self.config_handler.get_acquisition_duration_in_secs())  # Set default value
 
         # mu1 Frame
         mu1_frame = tk.Frame(parent_frame)
-        mu1_frame.grid(row=3, column=0, padx=10, pady=10)
+        mu1_frame.grid(row=0, column=2, padx=10, pady=10)
 
         mu1_label = tk.Label(mu1_frame, text="mu1")
         mu1_label.grid(row=0, column=0, padx=10, pady=10)
 
         self.mu1_edittext = tk.Entry(mu1_frame, name="mu1")
         self.mu1_edittext.bind('<FocusIn>', self.show_keyboard)
-        self.mu1_edittext.bind('<Button-1>', self.show_keyboard)
-        self.mu1_edittext.grid(row=0, column=1, padx=10, pady=10)
+        self.mu1_edittext.grid(row=0, column=3, padx=10, pady=10)
         self.mu1_edittext.insert(0, self.config_handler.get_mu1())  # Set default value
 
         # mu2 Frame
         mu2_frame = tk.Frame(parent_frame)
-        mu2_frame.grid(row=3, column=1, padx=10, pady=10)
+        mu2_frame.grid(row=1, column=2, padx=10, pady=10)
 
         mu2_label = tk.Label(mu2_frame, text="mu2")
         mu2_label.grid(row=0, column=0, padx=10, pady=10)
 
         self.mu2_edittext = tk.Entry(mu2_frame, name="mu2")
         self.mu2_edittext.bind('<FocusIn>', self.show_keyboard)
-        self.mu2_edittext.bind('<Button-1>', self.show_keyboard)
-        self.mu2_edittext.grid(row=0, column=1, padx=10, pady=10)
+
+        self.mu2_edittext.grid(row=0, column=3, padx=10, pady=10)
         self.mu2_edittext.insert(0, self.config_handler.get_mu2())  # Set default value
 
         # std1 Frame
         std1_frame = tk.Frame(parent_frame)
-        std1_frame.grid(row=4, column=0, padx=10, pady=10)
+        std1_frame.grid(row=2, column=2, padx=10, pady=10)
 
         std1_label = tk.Label(std1_frame, text="std1")
         std1_label.grid(row=0, column=0, padx=10, pady=10)
 
         self.std1_edittext = tk.Entry(std1_frame, name="std1")
         self.std1_edittext.bind('<FocusIn>', self.show_keyboard)
-        self.std1_edittext.bind('<Button-1>', self.show_keyboard)
-        self.std1_edittext.grid(row=0, column=1, padx=10, pady=10)
+
+        self.std1_edittext.grid(row=0, column=3, padx=10, pady=10)
         self.std1_edittext.insert(0, self.config_handler.get_std1())  # Set default value
 
         # std2 Frame
@@ -123,7 +105,6 @@ class ConfigPage(BaseScreen):
 
         self.std2_edittext = tk.Entry(std2_frame, name="std2")
         self.std2_edittext.bind('<FocusIn>', self.show_keyboard)
-        self.std2_edittext.bind('<Button-1>', self.show_keyboard)
         self.std2_edittext.grid(row=0, column=1, padx=10, pady=10)
         self.std2_edittext.insert(0, self.config_handler.get_std2())  # Set default value
 
@@ -133,7 +114,7 @@ class ConfigPage(BaseScreen):
         self.save_image = tk.PhotoImage(file=save_image_path)
         save_button = tk.Button(self, image=self.save_image, command=self.save_config, borderwidth=0, highlightthickness=0)
         save_button.image = self.save_image  # Keep a reference to avoid garbage collection
-        save_button.place(relx=0.30, rely=0.7)  # Center the button
+        save_button.place(relx=0.30, rely=0.575)  # Center the button
 
 
         #HOME
@@ -142,7 +123,7 @@ class ConfigPage(BaseScreen):
         self.home_image = tk.PhotoImage(file=home_image_path)
         home_button = tk.Button(self, image=self.home_image, command=self.back_to_home, borderwidth=0, highlightthickness=0)
         home_button.image = self.home_image  # Keep a reference to avoid garbage collection
-        home_button.place(relx=0.55, rely=0.7)  # Center the button
+        home_button.place(relx=0.55, rely=0.575)  # Center the button
 
 
     def show_entry_widgets(self, entry_widget):
@@ -154,7 +135,7 @@ class ConfigPage(BaseScreen):
         threshold_delta_total_value = self.threshold_delta_total_edittext.get()
         acquisition_duration_value = self.acquisition_duration_edittext.get()
         mu1 = self.mu1_edittext.get()
-        mu2 = self.mu1_edittext.get()
+        mu2 = self.mu2_edittext.get()
         std1 = self.std1_edittext.get()
         std2 = self.std2_edittext.get()
 
@@ -169,6 +150,18 @@ class ConfigPage(BaseScreen):
 
         # Show a messagebox to inform the user
         messagebox.showinfo("Info", "Configuration saved successfully!")
+
+    def show_keyboard(self, event):
+        if not self.keyboard_instance:
+            self.keyboard_instance = Keyboard(self, event.widget)
+            self.keyboard_instance.grid(row=1, column=0, columnspan=2, sticky="we")
+        elif not self.keyboard_instance.winfo_exists():  # Check if the widget still exists
+            self.keyboard_instance.destroy()  # Destroy the old instance
+            self.keyboard_instance = Keyboard(self, event.widget)  # Create a new instance
+            self.keyboard_instance.grid(row=1, column=0, columnspan=2, sticky="we")
+        widget_x = 10
+        widget_y = 400
+        self.keyboard_instance.show(widget_x, widget_y)
 
     def back_to_home(self):
         from .home_screen import HomeScreen  # Import inside the function
--- UIForWaterQualityRevised/screens/keyboard.py
+++ UIForWaterQualityRevised/screens/keyboard.py
@@ -1,185 +1,57 @@
-from tkinter import *  # import all module inside the tkinter
-from datetime import *
-import os
-from tkinter import messagebox as msg
 import tkinter as tk
-from PIL import ImageTk, Image
-
-exp = " "  # global variable
-flag = 0
 
 
-class my_key(tk.Frame):
-    def __init__(self, master, active_text_widget=None):
-        super().__init__(master)
-        print("Keyboard initialized")
-        self.active_text_widget = active_text_widget
-        self.exp = ""  # Replacing the global variable with an instance variable
-        test_button = tk.Button(self, text="Test", command=lambda: self.press("Test"))
-        test_button.pack()
-    def press(self, data):
-        self.exp += str(data)
+class Keyboard(tk.Frame):
+    def __init__(self, parent, text_widget):
+        super().__init__(parent)
+        self.text_widget = text_widget
+        self.create_keyboard()
 
-        if self.exp == ' ':
-            print("Don't use space at the initial")
-            self.exp = ''
+    def create_keyboard(self):
+        # Define the keyboard layout
+        keys = [
+            ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', 'Backspace', '='],
+            ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
+            ['Caps ', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'DONE'],
+            ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift', 'Space'],
+        ]
+
+        for i, row in enumerate(keys):
+            for j, key in enumerate(row):
+                button_height = 2
+                button_width = 2  # Set width for regular keys to 3 characters
+                column_span = 2
+                if len(key) > 2:
+                    button_height = 2
+                    button_width = 3  # Adjust the width for keys with names longer than two characters
+                button = tk.Button(self, text=key, command=lambda k=key: self.press(k), height=button_height,
+                                   width=button_width if key != 'Space' else 20)  # Adjust width for the space key
+                button.grid(row=i, column=j, sticky='nsew', padx=1, pady=1)
+                self.grid_columnconfigure(j, weight=1)
+            self.grid_rowconfigure(i, weight=1)
+
+    def press(self, key):
+        if key == 'Backspace':
+            if isinstance(self.text_widget, tk.Entry):
+                current_text = self.text_widget.get()
+                self.text_widget.delete(len(current_text) - 1)
+            else:  # Assuming it's a Text widget or similar
+                current_text = self.text_widget.get("1.0", tk.END)
+                self.text_widget.delete("1.0", tk.END)
+                self.text_widget.insert(tk.END, current_text[:-2])  # Remove the last character and newline
+        elif key == 'Space':
+            self.text_widget.insert(tk.END, ' ')
+        elif key == 'Enter':
+            self.text_widget.insert(tk.END, '\n')
+        elif key == 'DONE':
+            self.destroy()  # Close the keyboard
         else:
-            if self.active_text_widget:
-                self.active_text_widget.delete(0, tk.END)  # Clear the current content
-                self.active_text_widget.insert(tk.END, self.exp)  # Update with the new content
+            self.text_widget.insert(tk.END, key)
 
-    # ... (rest of the methods and logic for the keyboard layout/buttons) ...
+    def show(self, x, y):
+        # Position the keyboard at the given x and y coordinates
+        self.place(x=x, y=y)
 
-    def clear(self):
-        global exp
-        exp = exp[:-1]  ##remove the last character
-        self.active_text_widget.set(exp)
-
-    def submit(self, win, geo):
-        global exp
-        global flag
-        Temp_window = Toplevel(win)
-        Temp_window.geometry(geo)
-
-        backgroundImage = Image.open("../images/AreYouOkayToCreateNewSample.png")
-        resized_image = backgroundImage.resize((1030, 700), Image.ANTIALIAS)
-        backgroundImageResized = ImageTk.PhotoImage(resized_image)
-        backgroundImageLabel = Label(Temp_window, image=backgroundImageResized, bd=0, bg="#E6ECF0")
-        backgroundImageLabel.place(x=0, y=0)
-
-        # user_name = Label(Temp_window,text = "Are you okay to create new sample folder?",font=("Arial", 35)
-        #     ,bg=0,fg=0,)
-        # user_name.place(x = 60, y = 150)
-
-        num = Button(Temp_window, text='No',
-                     highlightthickness=0, bd=0,
-                     bg="#004d6c", fg="white",
-                     activeforeground="#ffffff", activebackground="#004d6c",
-                     font=('Arial', 30, 'bold'),
-                     width=10, height=4
-                     , command=lambda: self.Value_return(Temp_window, 0))
-        num.place(x=150, y=300)
-
-        num = Button(Temp_window, text='Yes',
-                     highlightthickness=0, bd=0,
-                     bg="#004d6c", fg="white",
-                     activeforeground="#ffffff", activebackground="#004d6c",
-                     font=('Arial', 30, 'bold'),
-                     width=10, height=4,
-                     command=lambda: self.NextStep(win))
-        num.place(x=600, y=300)
-
-        Temp_window.config(background="#E6ECF0")
-        Temp_window.mainloop()  # place window on computer screen, listen for events
-
-    def Button_template(self, win, text, argument, X, Y):
-        Button(win, text=text, highlightthickness=0, bd=0,
-               bg="#004d6c", fg="white",
-               activeforeground="#ffffff", activebackground="#004d6c",
-               font=('Arial', 30, 'bold'),
-               width=3, height=0
-               , command=lambda: self.press(argument)).place(x=X, y=Y)
-
-    def keyboard_button(self, win, geo):
-        global exp
-        exp = ''
-        individual_button_width = 4
-        individual_button_height = 3
-        sampleIDWindow = Toplevel(win)
-        sampleIDWindow.geometry(geo)
-
-        backgroundImage = Image.open("../images/background image 2.png")
-        resized_image = backgroundImage.resize((1030, 700), Image.ANTIALIAS)
-        backgroundImageResized = ImageTk.PhotoImage(resized_image)
-        backgroundImageLabel = Label(sampleIDWindow, image=backgroundImageResized, bd=0, bg="#E6ECF0")
-        backgroundImageLabel.place(x=0, y=0)
-
-        Dis_entry = Entry(sampleIDWindow, textvariable=self.active_text_widget, font=("Comic sans", 50))  ### entry Box
-        Dis_entry.place(x=100, y=100)
-
-        ##########NUMBER COLUMNM#######################
-
-        Row = 260
-        Button_Pos = 95
-        position = 50
-        letter = "1234567890"
-        for i in range(len(letter)):
-            self.Button_template(sampleIDWindow, letter[i], letter[i], position, Row)
-            position = Button_Pos + position
-
-        # ######################First Row for Letters##################
-        Row = Row + 60
-        Button_Pos = 95
-        position = 50
-        letter = "QWERTYUIOP"
-
-        for i in range(len(letter)):
-            self.Button_template(sampleIDWindow, letter[i], letter[i], position, Row)
-            position = Button_Pos + position
-
-        ### second row of letter##############
-        Row = Row + 60
-        Button_Pos = 95
-        position = 80
-
-        letter = "ASDFGHJKL"
-
-        for i in range(len(letter)):
-            self.Button_template(sampleIDWindow, letter[i], letter[i], position, Row)
-            position = Button_Pos + position
-
-        ### Third row of letter##############
-        Row = Row + 60
-        Button_Pos = 95
-        position = 150
-
-        letter = "ZXCVBNM"
-
-        for i in range(len(letter)):
-            self.Button_template(sampleIDWindow, letter[i], letter[i], position, Row)
-            position = Button_Pos + position
-
-        clear = Button(sampleIDWindow, text='Clear',
-                       highlightthickness=0, bd=0,
-                       bg="#004d6c", fg="white",
-                       activeforeground="#ffffff", activebackground="#004d6c",
-                       font=('Arial', 30, 'bold'),
-                       width=6, height=0,
-                       command=self.clear)
-        clear.place(x=position, y=Row)
-
-        ### Third row of letter##############
-        Row = Row + 60
-        Button_Pos = 95
-        position = 100
-        space_bar = Button(sampleIDWindow, text='SPACE',
-                           highlightthickness=0, bd=0,
-                           bg="#004d6c", fg="white",
-                           activeforeground="#ffffff", activebackground="#004d6c",
-                           font=('Arial', 30, 'bold'),
-                           width=20, height=0, command=lambda: self.press('_'))
-        space_bar.place(x=position, y=Row)
-
-        position = 700
-        destroy = Button(sampleIDWindow, text='Back to Menu',
-                         highlightthickness=0, bd=0,
-                         bg="#006A96", fg="white",
-                         activeforeground="#ffffff", activebackground="#006A96",
-                         font=('Arial', 30, 'bold'),
-                         width=12, height=0,
-                         # command=lambda:self.NextStep(sampleIDWindow,"BackToMenu"))
-                         command=sampleIDWindow.destroy)
-        destroy.place(x=position, y=0)
-
-        position = 600
-        enter = Button(sampleIDWindow, text='Submit',
-                       highlightthickness=0, bd=0,
-                       bg="#006A96", fg="white",
-                       activeforeground="#ffffff", activebackground="#006A96",
-                       font=('Arial', 30, 'bold'),
-                       width=12, height=0,
-                       command=lambda: self.submit(sampleIDWindow, geo))
-        enter.place(x=position, y=Row)
-
-        sampleIDWindow.config(background="#E6ECF0")
-        sampleIDWindow.mainloop()  # place window on computer screen, listen for events
+    def hide(self):
+        # Hide the keyboard
+        self.place_forget()
